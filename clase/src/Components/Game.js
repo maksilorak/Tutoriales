0cf017989943c31a91a75fddece6083323f88b4d
maksilorak <maksilorak@gmail.com>
2022-10-18 23:58:17 -0500

    Inicial

---
+++ clase/src/Components/Game.js
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react';
+//useState maneja el estado de un componente
+
+const [actores, setListadoActores] = useState([]); //se inicializa vacio actores
+
+
+const Game = () => {
+  return (
+    <div>
+      <h1>Actores</h1>
+    </div>
+  )
+}
+
+export default Game
